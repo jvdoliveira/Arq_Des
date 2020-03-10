89239b17f57d2d43ee9fe5ef4a7b0ac728acd051
João Victor da Silva Oliveira - 817122143 <817122143@animaedu.intranet>
2020-03-10 20:02:29 -0300

    Adicao das classes

---
+++ ArmazemNomesDesvirgulados.java
@@ -0,0 +1,16 @@
+package factory;
+
+import java.util.ArrayList;
+
+public class ArmazemNomesDesvirgulados extends ArmazemNomes {
+
+	public ArmazemNomesDesvirgulados(ArrayList<Nome> nomes) {
+		super(nomes);
+	}
+
+	@Override
+	protected CriadorDeNomes getCriador() {
+		return new CriadorDeNomesDesvirgulados();
+	}
+
+}
---
+++ CriadorDeNomes.java
@@ -0,0 +1,6 @@
+package factory;
+
+
+public interface CriadorDeNomes {
+	Nome criar(String s);
+}
